82647d7f1e01228892ca9127a7cf012c7f85a3a0
Egbon <egbon@SOSA-MacBook-Pro.local>
2025-03-14 23:11:40 +0100

    edit async again

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const catchAsync = require('../utils/catchasync');
+const catchasync = require('../utils/catchasync');
 const passport = require('passport');
 const { storeReturnTo } = require('../middleware/middlewares');
 const users = require('../controllers/users');
